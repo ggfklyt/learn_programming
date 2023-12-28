5ca6b74ec64e72ad76b9d99d22ec3b40f16cfc3e
nonsuch1 <artur.parskov1@gmail.com>
2023-12-28 21:48:46 +0300

    Makefile

---
+++ c/42/global/libft/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	printf(ft_strlen("abc"));
+}
--- c/42/global/libft/srcs/ft_strlcat.c
+++ c/42/global/libft/srcs/ft_strlcat.c
@@ -4,7 +4,7 @@ size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize
 {
 	size_t srclen = ft_strlen(src);
 	size_t dstlen = ft_strlen(dst);
-	size_t res = srclen + (dstsize > dstlen) ? dstlen : dstsize);
+	size_t res = srclen + (dstsize > dstlen ? dstlen : dstsize);
 	int append_size = dstsize  - dstlen - 1;
 	while (*dst) {
 		dst++;
