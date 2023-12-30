cca7bb72328f2318792987b0695d1eabb478b6c6
nonsuch1 <artur.parskov1@gmail.com>
2023-12-31 01:21:07 +0300

    fd

--- c/42/global/get_next_line/get_next_line.c
+++ c/42/global/get_next_line/get_next_line.c
@@ -1,4 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
 char *get_next_line(int fd)
 {
-	
+	char *buffer = malloc(120);
+	int n = read(fd, buffer, 120);
+	printf("%s", buffer);
+	return buffer;
 }
